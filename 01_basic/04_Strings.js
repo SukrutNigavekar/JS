fdcf9ba317a08e10de701ad06921aa8e7b56c7ca
SukrutNigavekar <sukrutedu@gmail.com>
2024-03-15 01:21:07 +0530

    String operations

---
+++ 01_basic/04_Strings.js
@@ -0,0 +1,14 @@
+const name = "Sukrut";
+const repoCount = 10;
+
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+let gameName = new String("Sukrut");
+
+console.log(gameName);
+// console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.indexOf('t'));
