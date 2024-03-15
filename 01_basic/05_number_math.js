122d6bb506feb4adb4f0bcc518953191814b59b3
SukrutNigavekar <sukrutedu@gmail.com>
2024-03-15 14:13:56 +0530

    Math and number ops

---
+++ 01_basic/05_number_math.js
@@ -0,0 +1,27 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const number = 123.8966;
+// console.log(number.toPrecision(4));
+const num2 = 1000000;
+// console.log(num2.toLocaleString());
+// console.log(num2.toLocaleString("en-IN"));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//          Maths
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(4.78));
+// console.log(Math.round(Math.random() * 10 + 1));
+
+let min = 10,
+  max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
